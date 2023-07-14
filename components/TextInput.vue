8e101fd268fbc008bab904675595be5f971c29a6
Nguyen-Tan-Phat <tanphat26112000@gmail.com>
2023-07-14 21:34:27 +0700

    first commit

---
+++ components/TextInput.vue
@@ -0,0 +1,30 @@
+<template>
+    <v-text-field
+      v-bind="$attrs"
+      v-on="$listeners"
+      v-model="inputValue"
+    ></v-text-field>
+  </template>
+  
+  <script>
+  import { VTextField } from 'vuetify/lib';
+  
+  export default {
+    extends: VTextField,
+    inheritAttrs: false,
+    data() {
+      return {
+        inputValue: this.value,
+      };
+    },
+    watch: {
+      inputValue(newValue) {
+        this.$emit('input', newValue);
+      },
+      value(newValue) {
+        this.inputValue = newValue;
+      },
+    },
+  };
+  </script>
+  
---
+++ pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-container class="ma-6">
+    <h1>Copy Text App</h1>
+    <text-input v-model="inputText"></text-input>
+    <v-btn color="primary" @click="copyToClipboard">Copy</v-btn>
+    <v-alert v-if="isCopied" type="success">Text copied!</v-alert>
+  </v-container>
+</template>
+
+<script>
+import TextInput from '~/components/TextInput.vue';
+
+export default {
+  components: {
+    TextInput,
+  },
+  data() {
+    return {
+      inputText: '',
+      isCopied: false,
+    };
+  },
+  methods: {
+    copyToClipboard() {
+      navigator.clipboard.writeText(this.inputText);
+      this.isCopied = true;
+      setTimeout(() => {
+        this.isCopied = false;
+      }, 2000);
+    },
+  },
+};
+</script>
